105990b10aa3f3fbcbd30cbbf47b9b50c0f61ef7
Suyeon Cho <suyeon4696@gmail.com>
2024-03-25 11:48:26 +0100

    Initial Commit

---
+++ AR Guide Service/AR_Guide_ServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Guide_ServiceApp.swift
+//  AR Guide Service
+//
+//  Created by Suyeon Cho on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AR_Guide_ServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
