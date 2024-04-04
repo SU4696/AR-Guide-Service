b369aaba0bf793b256ad969db73c42c0ed92c3be
Suyeon Cho <suyeon4696@gmail.com>
2024-04-04 09:44:46 +0200

    content view desifn fixed

--- AR Guide Service/Feature Targets/ContentView.swift
+++ AR Guide Service/Feature Targets/ContentView.swift
@@ -16,74 +16,89 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack{
-            NavigationLink{
-                CustomARViewRepresentable()
-                    .ignoresSafeArea()
-                    .overlay(alignment: .bottom){
-                        ScrollView(.horizontal){
-                            HStack{
-                                Button{
-                                    ARManager.shared.actionStream.send(.removeAllAnchors)
-                                }label: {
-                                    Image(systemName: "trash")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 40, height: 40)
-                                        .padding()
-                                        .background(.regularMaterial)
-                                        .cornerRadius(16)
-                                }
-                                
-                                ForEach(color, id: \.self) { color in
-                                    Button{
-                                        ARManager.shared.actionStream.send(.placeBlock(color: color))
-                                    }label: {
-                                        color
-                                            .frame(width: 40, height: 40)
-                                            .padding()
-                                            .background(.regularMaterial)
-                                            .cornerRadius(16)
+            ZStack{
+                Color.iconblue.ignoresSafeArea()
+                VStack (spacing: 30){
+                    Image(.eye)
+                        .resizable()
+                        .frame(width:100, height: 100)
+//                        .border(.white)
+                    NavigationLink{
+                        CustomARViewRepresentable()
+                            .ignoresSafeArea()
+                            .overlay(alignment: .bottom){
+                                ScrollView(.horizontal){
+                                    HStack{
+                                        Button{
+                                            ARManager.shared.actionStream.send(.removeAllAnchors)
+                                        }label: {
+                                            Image(systemName: "trash")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 40, height: 40)
+                                                .padding()
+                                                .background(.regularMaterial)
+                                                .cornerRadius(16)
+                                        }
+                                        
+                                        ForEach(color, id: \.self) { color in
+                                            Button{
+                                                ARManager.shared.actionStream.send(.placeBlock(color: color))
+                                            }label: {
+                                                color
+                                                    .frame(width: 40, height: 40)
+                                                    .padding()
+                                                
+                                            }
+                                            
+                                        }
+                                        
                                     }
                                 }
+                            }
+                    }label: {
+                        Text( "Add Box")
+                            .padding()
+                    }
+                    .foregroundColor(.iconblue)
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .clipShape(Capsule())
+                    NavigationLink{
+                        ZStack(alignment: .bottom) {
+                                                    CustomARViewETContainer(eyeGazeActive: $eyeGazeActive, lookAtPoint: $lookAtPoint, isWinking: $isWinking)
+                                                        .ignoresSafeArea()
+                            Button(action: {
+                                eyeGazeActive.toggle()
+                            }, label: {
+                                Text(eyeGazeActive ? "Stop" : "Start")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding()
                                 
+                            })
+                            
+                            .padding(.bottom, 50)
+                            
+                            if let lookAtPoint = lookAtPoint {
+                                Circle()
+                                    .fill(Color.pink)
+                                    .frame(width: isWinking ? 100 : 40, height: isWinking ? 100 : 40)
+                                    .position(lookAtPoint)
                             }
                         }
-                    }
-            }label: {
-                Text( "Add Box") .background(.regularMaterial)
-                    .cornerRadius(16)
-            }
-            .padding()
-            NavigationLink{
-                ZStack(alignment: .bottom) {
-                    CustomARViewETContainer(eyeGazeActive: $eyeGazeActive, lookAtPoint: $lookAtPoint, isWinking: $isWinking)
-                        .ignoresSafeArea()
-                    Button(action: {
-                        eyeGazeActive.toggle()
-                    }, label: {
-                        Text(eyeGazeActive ? "Stop" : "Start")
-                            .font(.headline)
-                            .foregroundColor(.white)
+                    }label: {
+                        
+                        Text( "Eye Tracking View")
                             .padding()
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                    })
-                    .padding(.bottom, 50)
-                    
-                    if let lookAtPoint = lookAtPoint {
-                        Circle()
-                            .fill(Color.pink)
-                            .frame(width: isWinking ? 100 : 40, height: isWinking ? 100 : 40)
-                            .position(lookAtPoint)
                     }
-                }
-            }label: {
-                
-                Text( "Eye Tracking View")
-                    .background(.regularMaterial)
-                    .cornerRadius(16)
+                    .foregroundColor(.iconblue)
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .clipShape(Capsule())
+                    
+                }  .fixedSize(horizontal: true, vertical: false)
             }
-            
         }
     }
 }
